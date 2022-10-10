dd756403ce0bdc16295eccf3ed4d42d114547d02
heavy02011 <heavy02011>
2022-10-10 20:24:46 +0200

    03 - cameras work with gstreamer, tensorflow needs python 3.6.x

---
+++ tests/test_tensorflow.py
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/55691174/check-whether-tensorflow-is-running-on-gpu
+
+import tensorflow as tf
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
